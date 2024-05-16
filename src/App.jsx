9a120b5685415224937cd38496cd7c5fafd7195d
mujahidtemp404 <mujahidislam400301@gmail.com>
2024-05-16 12:20:04 +0600

    add task title successfilly

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ const App = () => {
   };
 
   return (
-    <div className='w-11/12 m-auto text-[#000000]'>
+    <div className='w-11/12 h-screen m-auto text-[#000000] flex items-center justify-center'>
       <div className="container flex flex-col gap-3 p-4 items-center overflow-hidden mx-auto w-full md:max-w-md mt-8 bg-[#7AB2B2] rounded-md">
         <h1 className="md:text-3xl text-xl py-2 font-semibold flex w-full justify-center items-center gap-3">
           <img className='md:w-10 w-8' src={Icon} alt="icon" />
@@ -61,16 +61,18 @@ const App = () => {
           {tasks.map((task) => (
             <li
               key={task.id}
-              className={`flex items-center gap-2 w-full shadow-sm shadow-slate-400 h-auto justify-between border border-[#7AB2B2] rounded-md p-2 mb-2 ${
-                task.completed ? 'bg-gray-200' : 'bg-[#CDE8E5]'
-              }`}
+              className={`flex items-center gap-2 w-full shadow-sm shadow-slate-400 h-auto justify-between border border-[#7AB2B2] rounded-md p-2 mb-2 ${task.completed ? 'bg-gray-200' : 'bg-[#CDE8E5]'
+                }`}
             >
-              <p className={`cursor-pointer text-clip overflow-hidden basis-10/12 ${task.completed ? 'line-through' : 'animate-pulse'}`}>
+              <p
+                title={task.completed ? ' Task is completed' : 'The task remains'}
+                className={`cursor-pointer text-clip overflow-hidden basis-10/12 ${task.completed ? 'line-through' : 'animate-pulse'}`} style={{ transition: 'none' }}
+              >
                 {task.text}
               </p>
               <div className='flex flex-row justify-center gap-1 basis-2/12'>
                 <button onClick={() => toggleTaskCompletion(task.id)}>
-                <img className='md:w-6 w-4' src={task.completed ? checkIcon : blankIcon} alt="icon" />
+                  <img className='md:w-6 w-4' src={task.completed ? checkIcon : blankIcon} alt="icon" />
                 </button>
                 <button onClick={() => deleteTask(task.id)} className="text-red-500">
                   <img className='md:w-6 w-4' src={deleteIcon} alt="deleteIcon" />
